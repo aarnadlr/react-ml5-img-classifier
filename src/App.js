55d8edda3945779283e6fd001f4d2ab462b89305
aarnadlr <aaronadler@gmail.com>
2019-10-05 21:12:21 -0400

    Refactor App to function component

--- src/App.js
+++ src/App.js
@@ -1,31 +1,22 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import tiger from './img/tiger.jpg';
 import * as ml5 from 'ml5';
 
-class App extends Component {
+const App = () => {
+  const [predictionsArr, setPredictionsArr] = useState([]);
 
-  //1
-  state = {
-    predictionsArr: []
-  };
-
-  setPredictions = pred => {
-    // Set the prediction state with the model predictions
-    this.setState({
-      predictionsArr: pred
-    });
-  };
-
-  classifyImg = () => {
+  const classifyImg = () => {
     // Initialize the Image Classifier method with MobileNet
     const classifier = ml5.imageClassifier('MobileNet', modelLoaded);
     // When the model is loaded
     function modelLoaded() {
       console.log('Model Loaded!');
     }
+
     // Put the image to classify inside a variable
     const image = document.getElementById('image');
+
     // Make a prediction with a selected image
     classifier
       .predict(image, 3, function(err, results) {
@@ -34,42 +25,43 @@ class App extends Component {
       })
       .then(results => {
         // Set the predictions in the state
-        this.setPredictions(results);
+        setPredictionsArr(results);
       });
   };
 
-  componentDidMount() {
-    // once the component has mount, start the classification
-    this.classifyImg();
+  useEffect(() => {
+    classifyImg();
+  }, []);
+
+  // First set the predictions to a default value while loading
+
+
+  let predictions = <div className="loader"/>;
+
+
+  // Map over the predictions and return each prediction with probability
+  if (predictionsArr.length > 0) {
+    predictions = predictionsArr.map((pred, i) => {
+
+      // round the probability with 2 decimal
+      // probability = Math.floor(probability * 10000) / 100 + "%";
+      return (
+        // Please wait a few seconds as the image is parsed
+
+        <div key={i + ''}>
+          {i + 1}. Prediction: {JSON.stringify(pred)}{' '}
+        </div>
+      );
+    });
   }
 
-  render() {
-    // First set the predictions to a default value while loading
-    let predictions = <div className="loader"></div>;
-    // Map over the predictions and return each prediction with probability
-    if (this.state.predictionsArr.length > 0) {
-      predictions = this.state.predictionsArr.map((pred, i) => {
-
-        // let { className, probability } = pred;
-        // round the probability with 2 decimal
-        // probability = Math.floor(probability * 10000) / 100 + "%";
-
-        return (
-          <div key={i + ''}>
-            {i + 1}. Prediction: {JSON.stringify(pred)}{' '}
-          </div>
-        );
-      });
-    }
-
-    return (
-      <div className="App">
-        <h1>Image classification with ML5.js</h1>
-        <img src={tiger} id="image" width="400" alt="" />
-        {predictions}
-      </div>
-    );
-  }
-}
+  return (
+    <div className="App">
+      <h1>Image classification with ML5.js</h1>
+      <img src={tiger} id="image" width="400" alt="" />
+      {predictions}
+    </div>
+  );
+};
 
 export default App;
